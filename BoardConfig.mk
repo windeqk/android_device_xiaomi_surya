surya-devs/device_xiaomi_surya

surya: Use clang 12.0.7 for kernel compilation
Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 96
 
 # Kernel
+TARGET_KERNEL_CLANG_VERSION := r416183d
 TARGET_KERNEL_CONFIG := surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 

windeqk/android_device_xiaomi_surya

BoardConfig.mk
@@ -29,6 +29,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya
 TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
 
 # Kernel
+TARGET_KERNEL_CLANG_VERSION := r416183b1
 TARGET_KERNEL_CONFIG := surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 