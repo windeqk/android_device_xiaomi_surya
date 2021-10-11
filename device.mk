33d0dea382f433cc1016743e364b45ab66d194ee
TH779 <i@779.moe>
2021-10-11 14:35:34 +0000

    surya: Take log without ADB
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Id107f4cc56c21d51d313beeb2638b551fa183f21

--- device.mk
+++ device.mk
@@ -267,6 +267,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Logs
+PRODUCT_PACKAGES += \
+    init.logcat.rc
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
